466ad4382cfb21f9f8ebb70b05c4a2caf9d29ad6
cedric e goddard <8ceg6@comcast.net>
2019-09-29 16:59:51 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
   when 2
     string = array[0] + " and " + array[1]
   else
-    string = String.new
+    string = string.new
     array.each_with_index do |item, index|
       string += "and " + item if index == array.size - 1
       break if index == array.size - 1
